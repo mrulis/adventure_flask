28b52ff8a79946a828c6cff4e3160e99c844c76b
mrulis <mrulis@udel.edu>
2019-10-30 21:24:13 -0400

    final project, only relevant files

--- views.py
+++ views.py
@@ -11,130 +11,75 @@ app = Flask(__name__)
 def hello(blog: dict) -> str:
     """
     The welcome screen for the game.
-
-    :param blog: The current world
-    :return: The HTML to show the player
+    Where you choose what kind of blog to make.
     """
     return render_template('index.html')
 
 
 @simple_route('/initial/')
 def set_blogname(blog: dict, *args):
+    """
+    Where you input blog title.
+    """
     blog['blog_type'] = request.values.get('blog_type')
     return render_template('blog_title.html', blog = blog)
 
 
 @simple_route('/set_title/')
 def set_title(blog: dict, *args):
+    """
+    Where blog title is set as value in blog dictionary.
+    """
     blog['title'] = request.values.get('title')
     return redirect('/picture_bank/')
 
 
 @simple_route('/picture_bank/')
 def set_pictures(blog: dict, *args):
+    """
+    Where you choose from set of pictures (options determined by blog type).
+    When you delete a post, resets caption value to None so you can inout anew caption later.
+    """
+    blog['caption'] = None
     return render_template('picture_bank.html', blog = blog)
 
 
 @simple_route('/set_picture/')
 def set_picture(blog: dict, *args):
+    """
+    Where chosen picture is set as value in blog dictionary.
+    """
     blog['picture'] = request.values.get('image')
+    return redirect('/blog_page/')
+
+
+@simple_route('/set_caption/')
+def set_caption(blog: dict, *args):
+    """
+    Where blog caption is set as value in blog dictionary.
+    """
+    blog['caption'] = request.values.get('caption')
     return redirect('/blog_page/')
 
 
 @simple_route('/blog_page/')
 def make_page(blog: dict, *args):
-    blog['caption'] = request.values.get('caption')
+    """
+    Where blog page is.
+    Can add caption to chosen photo.
+    Can choose to add text post.
+    """
     blog['text_post'] = request.values.get('text_post')
     return render_template('blog_page.html', blog = blog)
 
 
 @simple_route('/text_post/')
 def set_text_post(blog: dict, *args):
+    """
+    Where you can input text post.
+    """
     return render_template('text_post.html', blog = blog)
 
-
-@simple_route('/set_text/')
-def set_text(blog: dict, *args):
-    blog['text_post'] = request.values.get('text_post')
-    return redirect('/blog_page/')
-
-
-
-@simple_route('/aesthetic_picture_bank/')
-def set_aesthetic_pictures(blog: dict, *args):
-    return render_template('aesthetic_picture_bank.html', blog = blog)
-
-
-@simple_route('/aesthetic_pic_1/')
-def aesthetic_blog_1(blog: dict, *args):
-    blog['caption'] = request.values.get('caption')
-    return render_template('aesthetic_pic_1.html', blog = blog)
-
-
-@simple_route('/aesthetic_pic_2/')
-def aesthetic_blog_2(blog: dict, *args):
-    blog['caption'] = request.values.get('caption')
-    return render_template('aesthetic_pic_2.html', blog = blog)
-
-
-@simple_route('/aesthetic_pic_3/')
-def aesthetic_blog_3(blog: dict, *args):
-    blog['caption'] = request.values.get('caption')
-    return render_template('aesthetic_pic_3.html', blog = blog)
-
-
-
-@simple_route('/food/')
-def set_food_blogname(blog: dict):
-    return render_template('food_initial.html')
-
-
-@simple_route('/food_picture_bank/')
-def set_food_pictures(blog: dict, *args):
-    blog['title'] = request.values.get('title')
-    return render_template('food_picture_bank.html', blog = blog)
-
-
-@simple_route('/food_pic_1/')
-def food_blog_1(blog: dict):
-    return render_template('food_pic_1.html', blog = blog)
-
-
-@simple_route('/food_pic_2/')
-def food_blog_2(blog: dict):
-    return render_template('food_pic_2.html', blog = blog)
-
-
-@simple_route('/food_pic_3/')
-def food_blog_3(blog: dict):
-    return render_template('food_pic_3.html', blog = blog)
-
-
-
-@simple_route('/travel/')
-def set_travel_blogname(blog: dict):
-    return render_template('travel_initial.html')
-
-
-@simple_route('/travel_picture_bank/')
-def set_travel_pictures(blog: dict, *args):
-    blog['title'] = request.values.get('title')
-    return render_template('travel_picture_bank.html', blog = blog)
-
-
-@simple_route('/travel_pic_1/')
-def travel_blog_1(blog: dict):
-    return render_template('travel_pic_1.html', blog = blog)
-
-
-@simple_route('/travel_pic_2/')
-def travel_blog_2(blog: dict):
-    return render_template('travel_pic_2.html', blog = blog)
-
-
-@simple_route('/travel_pic_3/')
-def travel_blog_3(blog: dict):
-    return render_template('travel_pic_3.html', blog = blog)
 
 @simple_route("/save/title/")
 def save_name(blog: dict, blog_name: str) -> str:
